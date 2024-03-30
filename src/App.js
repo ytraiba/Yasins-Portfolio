338137cdecedaa6e7b2164f35ca3b817327e6197
ytraiba <yasintraiba@gmail.com>
2024-03-30 10:41:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import './App.scss'
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-        </Route>
+    // <>
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route index element={<Home />} />
+    //       <Route path="about" element={<About />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //       <Route path="/projects" element={<Projects />} />
+    //     </Route>
         
-      </Routes>
-    </>
+    //   </Routes>
+    // </>
+    <div>Under maintenance</div>
   )
 }
 
